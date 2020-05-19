23ab238241fe41a961dd525af9ea583ac74f49bb
Tyler Holcombe <tyler@tessatech.org>
2020-05-19 16:23:15 -0700

    Updating create-league page. Leaving in temp variables for now.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { AuthGuard } from './login/auth.guard';
 import { HomeComponent } from './home/home.component';
+import { CreateLeagueComponent } from './leagues/create-league/create-league.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
-	{path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
   {path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
   {path: 'signup', component: SignupComponent},
+  {path: 'leagues/new', component: CreateLeagueComponent},
 ];
 
 @NgModule({
--- src/app/leagues/create-league/create-league.component.ts
+++ src/app/leagues/create-league/create-league.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-create-league',
@@ -6,10 +10,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-league.component.css']
 })
 export class CreateLeagueComponent implements OnInit {
+  // TODO implement max lengths for free text fields
+  private maxLeagueNameLength: number = environment.maxUsernameLength;
+  private leagueNameRegex: string = environment.usernameRegex;
+  private maxLeagueDescriptionLength: number = 500;
+
+  newLeagueModel: NewLeagueModel = new NewLeagueModel();
+
+  newLeagueForm = new FormGroup({
+    'name': new FormControl('', [Validators.required, Validators.maxLength(this.maxLeagueNameLength), Validators.pattern(this.leagueNameRegex)]),
+    'description': new FormControl('', [Validators.required, Validators.maxLength(this.maxLeagueDescriptionLength)]),
+    'team-size': new FormControl('', [Validators.required]),
+  },
+  );
+
+  get name() {
+    return this.newLeagueForm.get('name');
+  }
+
+  get description() {
+    return this.newLeagueForm.get('description');
+  }
+
+  get teamSize() {
+    return this.newLeagueForm.get('team-size');
+  }
 
   constructor() { }
 
   ngOnInit() {
   }
 
+}
+
+class NewLeagueModel {
+  errorMessage?: string;
+
+  clearErrors(): void {
+    this.errorMessage = null;
+  }
 }
